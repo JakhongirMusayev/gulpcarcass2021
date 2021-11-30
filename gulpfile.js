4677784f4c061a7cfc4a8b54b49771314b259b77
Jahkongir Musayev <DevDar76uzb@gmail.com>
2021-11-30 20:56:19 +0500

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
-const { series } = require("gulp");
-const sass = require("sass");
+const { series, src, dest } = require("gulp");
 const del = require("del");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
+const path = {
+  html: {
+    src: "src/html/**/*.html",
+    dest: "./dist/",
+  },
+  styles: {
+    src: "src/styles/**/*.scss",
+    dest: "./dist/css",
+  },
+  scripts: {
+    src: "src/scripts/**/*.js",
+    dest: "./dist/js",
+  },
+};
+//todo Clean
 function clean() {
-  return del(["build"]);
+  return del(["dist"]);
+}
+//todo Style
+function styles() {
+  return src(path.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      basename: "main",
+      suffix: ".min"
+    }))
+    .pipe(dest(path.styles.dest));
 }
 
-exports.default = series(clean);
+exports.clean = clean;
+exports.styles = styles;
+
+// exports.default = series(clean, styles);
