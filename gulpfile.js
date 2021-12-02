6eb8bf55cb874adda7d75950222d6eab9be97cdc
JakhongirMusayev <DevDar76uzb@gmail.com>
2021-12-02 15:19:58 +0500

    clean, html, scss, browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-const { series, src, dest } = require("gulp");
+const { series, src, dest, watch, parallel } = require("gulp");
 const del = require("del");
+const fileInclude = require("gulp-file-include");
+const htmlmin = require("gulp-htmlmin");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
 
 const path = {
   html: {
-    src: "src/html/**/*.html",
+    /* faqat "src/*.html" fayllarni "dest"ga o'tqazmoqdamiz, 
+    "src/_*.html" fayllarni esa o'tishlarini inkor ("!") qilmoqdamiz*/
+    src: ["src/*.html", "!" + "src/_*.html"],
     dest: "./dist/",
   },
   styles: {
@@ -20,21 +25,47 @@ const path = {
 };
 //todo Clean
 function clean() {
-  return del(["dist"]);
+  return del(["dist/*"]);
+}
+//todo Html
+function html() {
+  return src(path.html.src)
+    // .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(fileInclude())
+    .pipe(dest(path.html.dest))
+    .pipe(browserSync.stream());
 }
 //todo Style
 function styles() {
   return src(path.styles.src)
     .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(rename({
-      basename: "main",
-      suffix: ".min"
-    }))
-    .pipe(dest(path.styles.dest));
+    // .pipe(cleanCSS())
+    .pipe(
+      rename({
+        basename: "main",
+        suffix: ".min",
+      })
+    )
+    .pipe(dest(path.styles.dest))
+    .pipe(browserSync.stream());
 }
 
-exports.clean = clean;
-exports.styles = styles;
+function watching() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+      open: false,
+      // online: false,
+      // notify: false
+    }
+  });
+  watch(path.html.src, html);
+  watch(path.styles.src, styles);
+}
 
-// exports.default = series(clean, styles);
+// exports.clean = clean;
+// exports.html = html;
+// exports.styles = styles;
+// exports.watching = watching;
+
+exports.default = series(clean, html, styles, watching);
